4c2b16331518f20c70b37e24cfef0b81ae4fe3d2
Toba Folaranmi <ofolaranmi@companieshouse.gov.uk>
2023-08-07 15:20:20 +0100

    refactoring cluster module

--- groups/infrastructure/module-secrets/parameters.tf
+++ groups/infrastructure/module-secrets/parameters.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "secret_parameters" {
-  for_each    = nonsensitive(var.secrets)
+  for_each = var.secrets
+
   name        = "/${var.name_prefix}/${each.key}"
   key_id      = var.kms_key_id
   description = each.key
